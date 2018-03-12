b1c82550d9b27f411f7132765382e2e116d6d626
William Hogg <naturesyouth@gmail.com>
2018-03-12 19:38:03 +0000

    fixes formatting

--- stream-data.py
+++ stream-data.py
@@ -9,7 +9,7 @@ def main():
         o = s.get_orientation()
         print("pitch: {:3.2f}, roll: {:3.2f}, y: {:3.2f}".format(o['pitch'], o['roll'], a['yaw']))
         a = s.get_accelerometer_raw
-        print("x: {:3.2f}, y: {:3.2f}, z: {:3.2f}".format(a['x'], a['y'], a['z']))
+        print("x: {:03.2f}, y: {:03.2f}, z: {:03.2f}".format(a['x'], a['y'], a['z']))
 
 if __name__ == '__main__':
     main()
