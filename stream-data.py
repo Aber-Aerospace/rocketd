180695b506da0feffe5905c2c008e64b0819352f
William Hogg <naturesyouth@gmail.com>
2018-03-12 19:56:41 +0000

    adds test for chute.

--- stream-data.py
+++ stream-data.py
@@ -1,4 +1,5 @@
 import time
+import math
 from sense_hat import SenseHat
 
 s = SenseHat()
@@ -7,9 +8,13 @@ s = SenseHat()
 def main():
     while True:
         o = s.get_orientation()
+
+        if(5 > 0['pitch'] or o['pitch'] > 355):
+            print("CHUTE")
+        else:
+            print("")
+
         print("pitch: {:3.2f}, roll: {:3.2f}, y: {:3.2f}".format(o['pitch'], o['roll'], a['yaw']))
-        a = s.get_accelerometer_raw
-        print("x: {:03.2f}, y: {:03.2f}, z: {:03.2f}".format(a['x'], a['y'], a['z']))
 
 if __name__ == '__main__':
     main()
